a286adcdeb15b05a58e982885481183c7c7046bd
TheBlackParrot <theblackparrot@rocketmail.com>
2015-06-01 00:50:44 -0500

    dialog animations

---
+++ js/dialog.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$("#openDialog").on("click",function(){
+		var loadSpot = $(".dialog-load-spot");
+		loadSpot.empty();
+		loadSpot.load("includes/test-dialog.html", function(){
+			$(".dialog").addClass("dialog-open");
+			$(".dialog-wrapper").addClass("standard-fadein");
+		});
+	})
+	$("#closeDialog").on("click",function(){
+		var dialog = $(this).parent().parent();
+		dialog.removeClass("dialog-open");
+		dialog.addClass("dialog-close");
+
+		var wrapper = dialog.parent();
+		wrapper.removeClass("standard-fadein");
+		wrapper.addClass("standard-fadeout");
+
+		dialog.one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+			wrapper.remove();
+		});
+	});
+});
---
+++ js/ui-interaction.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+	console.log("Ready.");
+	$("#toggleInfo, #toggleMain").on("click",function(){
+		console.log("Info toggle clicked.");
+		if($(".info-area").attr("visible") == 1) {
+			console.log("Info area is marked as visible.");
+			$(".info-area").removeClass("main-frame_slideUp");
+			$(".info-area").addClass("main-frame_slideDown");
+			$(".bg_img_info").removeClass("main-frame_slideUp");
+			$(".bg_img_info").addClass("main-frame_slideDown");
+			$(".info-area").one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+				$(".info-area").hide();
+				$(".info-area").attr("visible","0");
+				$(".bg_img_info").hide();
+				$(".bg_img_info").attr("visible","0");
+			});
+
+		} else {
+			console.log("Info area is marked as invisible.");
+			$(".info-area").show();
+			$(".info-area").attr("visible","1");
+			$(".info-area").removeClass("main-frame_slideDown");
+			$(".info-area").addClass("main-frame_slideUp");
+			$(".bg_img_info").show();
+			$(".bg_img_info").attr("visible","1");
+			$(".bg_img_info").removeClass("main-frame_slideDown");
+			$(".bg_img_info").addClass("main-frame_slideUp");
+		}
+	});
+
+	$(".user-header, #toggleUser").on("click",function(){
+		console.log("Account info toggle clicked.");
+		if($(".user-wrapper").attr("visible") == 1) {
+			console.log("User account area is marked as visible.");
+			$(".user-wrapper").removeClass("user_slideUp");
+			$(".user-wrapper").addClass("user_slideDown");
+			$(".user-caret").removeClass("rotatef");
+			$(".user-caret").addClass("rotateb");
+			$(".user-wrapper").one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
+				$(".user-wrapper").attr("visible","0");
+			});
+		} else {
+			console.log("User account area is marked as invisible.");
+			$(".user-wrapper").attr("visible","1");
+			$(".user-wrapper").removeClass("user_slideDown");
+			$(".user-wrapper").addClass("user_slideUp");
+			$(".user-caret").removeClass("rotateb");
+			$(".user-caret").addClass("rotatef");
+		}
+	});
+});
