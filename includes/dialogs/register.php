3765b8fc39494263e3e784bc8a3815e08900e773
TheBlackParrot <theblackparrot@rocketmail.com>
2015-06-03 23:13:45 -0500

    bugfixes to register dialog

--- includes/dialogs/register.php
+++ includes/dialogs/register.php
@@ -4,9 +4,19 @@
 ?>
 
 <span class="header">Register for <?php echo $prog_title; ?></span>
-<form action="includes/register.php" method="POST">
+<form action="includes/register.php" method="POST" id="register-info">
 	<input name="username" type="text" placeholder="Username"/><br/>
 	<input name="email" type="text" placeholder="Email"/><br/>
 	<input name="password" type="password" placeholder="Password"/><br/>
-	<input type="submit" value="Submit"/>
 </form>
+<div class="dialog-buttons">
+	<div class="button" onClick="submitRegister();">Register</div>
+	<div class="button" id="closeDialog">Cancel</div>
+</div>
+
+<script src="js/dialog.js"></script>
+<script>
+function submitRegister() {
+	document.getElementById("login-info").submit();
+}
+</script>
